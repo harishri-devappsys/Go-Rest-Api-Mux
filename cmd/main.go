dce848482aa387c47aa707914b88112fb926f37a
harishri-devappsys <harishri@devappsys.com>
2025-06-04 15:27:39 +0530

    Delete api added

--- cmd/main.go
+++ cmd/main.go
@@ -15,6 +15,7 @@ func main() {
 	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
 	router.HandleFunc("/books/{id}", handlers.Getbook).Methods(http.MethodGet)
 	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
 	log.Println("Api is running")
 	http.ListenAndServe(":4000", router)
 
